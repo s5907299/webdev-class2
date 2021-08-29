c8fe935ca9c18c6255d4539c5913e6eb1071e541
poomweb <s5907299>
2021-08-29 20:37:30 +0700

    2

--- app/controllers/ex2_controller.rb
+++ app/controllers/ex2_controller.rb
@@ -2,19 +2,6 @@ class Ex2Controller < ApplicationController
   def loop
     @a = params[:a]
     @b = params[:b]
-    if @a.class != Integer && @b.class != Integer
-      @c = 3
-    end
-    elsif @b.class != Integer
-      @c = 2
-    end
-    elsif @b.class != Integer
-      @c = 1
-    end 
-    else
-      @c = 0
-    end
-    puts @c
   end
   
 end
